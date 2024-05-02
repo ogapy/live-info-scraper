5434c3a1f91eebb4d96e6c262a9f2315bdd394f6
ogapy <kyon6690@gmail.com>
2024-05-02 15:23:45 +0900

    Refactor: Delegate detail extraction to LiveDetailsExtractor
    Why:
    - To adhere to the Single Responsibility Principle by separating data extraction logic from the EPlusScraper class, enhancing clarity and maintainability.
    
    What:
    - Extracted detail extraction methods from EPlusScraper and moved them to LiveDetailsExtractor.
    - Adjusted EPlusScraper to initialize and utilize LiveDetailsExtractor for data extraction tasks.
    - Removed obsolete extraction methods and imports from EPlusScraper.

--- src/repository/scraping/eplus/eplus_search.py
+++ src/repository/scraping/eplus/eplus_search.py
@@ -1,9 +1,9 @@
-import re
 from datetime import datetime
 
-from Entity.live import Live, Prefecture, RawLiveInfo
+from Entity.live import Live
 from Entity.ticket import Ticket
 from repository.scraping.browser_manager import BrowserManager, SearchBy
+from repository.scraping.eplus.live_detail_extractor import LiveDetailsExtractor
 from scraper import Scraper
 from util.date_range import DateRange
 
@@ -12,6 +12,7 @@ class EPlusScraper(Scraper):
     def __init__(self, url: str):
         super().__init__(url)
         self.browser_manager = BrowserManager(url)
+        self.live_details_extractor = LiveDetailsExtractor(self.browser_manager)
 
     def search_live(self, artist: str):
         self._move_artist_search_result(artist)
@@ -26,7 +27,7 @@ class EPlusScraper(Scraper):
 
     def _scan_lives_url(self):
         return [
-            self.browser_manager.get_attribute(live_link, "href")
+            self.browser_manager.get_element_attribute(live_link, "href")
             for live_link in self.browser_manager.find_elements(
                 SearchBy.CSS_SELECTOR,
                 "a[href*='/sf/detail/']",
@@ -38,7 +39,7 @@ class EPlusScraper(Scraper):
 
     def _scan_live(self, live_detail_url: str, index: int):
         # ライブの一覧ページにいます
-        raw_live_infos = self._scan_live_details(index + 1)
+        raw_live_infos = self.live_details_extractor.extract_live_details(index)
         # tickets = self._scan_tickets_details(live_detail_url, browser)
 
         self.browser_manager.get(live_detail_url)
@@ -63,56 +64,3 @@ class EPlusScraper(Scraper):
         )
         self.browser_manager.back()
         return live
-
-    def _scan_live_details(self, index: int):
-        venue_text = self.browser_manager.find_element(
-            SearchBy.CSS_SELECTOR,
-            f"a[href*='/sf/detail/']:nth-child({index}) .ticket-item__venue",
-        ).text
-        raw_date_range = self.browser_manager.find_element(
-            SearchBy.CSS_SELECTOR,
-            f"a[href*='/sf/detail/']:nth-child({index}) .ticket-item__left",
-        ).text
-        start_date, end_date = self._extract_date_range(raw_date_range)
-
-        return RawLiveInfo(
-            name=self.browser_manager.find_element(
-                SearchBy.CSS_SELECTOR,
-                f"a[href*='/sf/detail/']:nth-child({index}) .ticket-item__title",
-            ).text,
-            raw_date_range=raw_date_range,
-            date_range=DateRange(
-                start_date=start_date,
-                end_date=end_date,
-            ),
-            prefecture=Prefecture(self._extract_prefecture(venue_text))
-            if self._extract_prefecture(venue_text)
-            else Prefecture.Unknown,
-            venue=self._extract_venue(venue_text),
-        )
-
-    def _extract_date_range(self, raw_date_range: str):
-        dates = raw_date_range.split("\n")
-        if len(dates) == 1:
-            start_date = end_date = self._parse_date(dates[0])
-        elif len(dates) == 2:
-            start_date = self._parse_date(dates[0])
-            end_date = self._parse_date(dates[1])
-        else:
-            raise ValueError("日付の形式が正しくありません。")
-
-        return start_date, end_date
-
-    def _parse_date(self, date_str):
-        year, month, day = map(
-            int, re.search(r"(\d{4})/(\d{1,2})/(\d{1,2})", date_str).groups()
-        )
-        return datetime(year, month, day)
-
-    def _extract_prefecture(self, venue_text: str):
-        match = re.search(r"\（([^）]+)\）", venue_text)
-        return match.group(1) if match else None
-
-    def _extract_venue(self, venue_text: str):
-        result = re.sub(r"\（[^）]*\）", "", venue_text)
-        return result.strip()
---
+++ src/repository/scraping/eplus/live_detail_extractor.py
@@ -0,0 +1,76 @@
+import re
+from datetime import datetime
+
+from Entity.live import Prefecture, RawLiveInfo
+from util.date_range import DateRange
+
+from repository.scraping.browser_manager import SearchBy
+
+
+class LiveDetailsExtractor:
+    def __init__(self, browser_manager):
+        self.browser_manager = browser_manager
+
+    def extract_live_details(self, index) -> RawLiveInfo:
+        nth_child_index = index + 1
+        venue_text = self._extract_venue_text(nth_child_index)
+        raw_date_range = self._extract_raw_date_range(nth_child_index)
+
+        return RawLiveInfo(
+            name=self._extract_name(nth_child_index),
+            raw_date_range=raw_date_range,
+            date_range=self._parse_date_range(raw_date_range),
+            prefecture=self._parse_prefecture(venue_text),
+            venue=self._parse_venue(venue_text),
+        )
+
+    def _extract_venue_text(self, nth_child_index: int) -> str:
+        return self.browser_manager.find_element(
+            SearchBy.CSS_SELECTOR,
+            f"a[href*='/sf/detail/']:nth-child({nth_child_index}) .ticket-item__venue",
+        ).text
+
+    def _extract_raw_date_range(self, nth_child_index: int) -> str:
+        return self.browser_manager.find_element(
+            SearchBy.CSS_SELECTOR,
+            f"a[href*='/sf/detail/']:nth-child({nth_child_index}) .ticket-item__left",
+        ).text
+
+    def _extract_name(self, nth_child_index: int) -> str:
+        return self.browser_manager.find_element(
+            SearchBy.CSS_SELECTOR,
+            f"a[href*='/sf/detail/']:nth-child({nth_child_index}) .ticket-item__title",
+        ).text
+
+    def _parse_date_range(self, raw_date_range: str) -> DateRange:
+        def _parse_date(date: str) -> datetime:
+            year, month, day = map(
+                int, re.search(r"(\d{4})/(\d{1,2})/(\d{1,2})", date).groups()
+            )
+
+            return datetime(year, month, day)
+
+        dates = raw_date_range.split("\n")
+        if len(dates) == 1:
+            start_date = end_date = _parse_date(dates[0])
+            return DateRange(
+                start_date=start_date,
+                end_date=end_date,
+            )
+        elif len(dates) == 2:
+            return DateRange(
+                start_date=_parse_date(dates[0]),
+                end_date=_parse_date(dates[1]),
+            )
+        else:
+            raise ValueError(
+                f"日付の形式が正しくありません。raw_date_range: {raw_date_range}"
+            )
+
+    def _parse_prefecture(self, venue_text: str) -> Prefecture:
+        match = re.search(r"\（([^）]+)\）", venue_text)
+        return Prefecture(match.group(1)) if match else Prefecture.Unknown
+
+    def _parse_venue(self, venue_text: str):
+        result = re.sub(r"\（[^）]*\）", "", venue_text)
+        return result.strip()
